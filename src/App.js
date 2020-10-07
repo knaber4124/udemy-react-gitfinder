2c30dad2c81fc3ffa2014e2dded3e14c23fb703a
Keith Naber <knaber4124@gmail.com>
2020-10-07 16:08:08 -0400

    initial commit, started navbar and users

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar/Navbar';
+import Users from './components/users/Users'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+
+  }
+}
+
+export default App;
