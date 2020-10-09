4e96eb3dd818f5f649eb187f441f223e6a31bb18
Keith Naber <knaber4124@gmail.com>
2020-10-09 15:04:23 -0400

    clear and started router

---
+++ src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About this Page</h1>
+            <p>Use this to search GitHub users</p>
+        </div>
+    )
+}
+
+export default About
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
     }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
+    }
+
     onChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
     onSubmit = (event) => {
         event.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter a search query', 'light')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
+
     }
 
     render() {
+        const { showClear, clearUsers } = this.props
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
@@ -20,6 +36,9 @@ export class Search extends Component {
                         onChange={this.onChange} />
                     <input type='submit' value="Search" className='btn btn-dark btn-block' />
                 </form>
+                {showClear && <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>
+                }
+
             </div>
         )
     }
